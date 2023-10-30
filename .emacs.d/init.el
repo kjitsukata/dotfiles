ed22c68d6e8b899ee54e5af5aa550f2ee88c554e
Kazuyuki Jitsukata <kjitsukata@gmail.com>
2023-10-30 18:29:48 +0900

    [add] Add frame size, position, and font settings for Emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,12 @@
+;; Setting frame size and frame position
+(setq initial-frame-alist
+	(append (list
+		'(width . 150)
+		'(height . 55)
+		'(top . 0)
+		'(left . 0)
+		'(font . "Cascadia Code")
+		)
+		initial-frame-alist))
+(setq default-frame-alist initial-frame-alist)
+
